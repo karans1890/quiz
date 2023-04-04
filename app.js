50f0fb2732fd6dafc81c1dc19e0104a3417912a7
Karan singh <103563328+karans1890@users.noreply.github.com>
2023-04-04 16:36:31 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,85 @@
+const quizData = [{ question : 'what is my fav thing to do in my free time ?',
+a:'playing games',
+b:'go outside',
+c:'listing music',
+d:'sleeping',
+correct:'c'
+},
+{ question : 'what is my fav color ?',
+a:'white',
+b:'green',
+c:'black',
+d:'red',
+correct:'a'
+},
+{ question: 'what is my Birth date ',
+a:'19/11/2002',
+b:'19/09/2000',
+c:'15/08/2002',
+d:'19/10/2002',
+correct:'d'
+}
+];
+let index = 0;
+let correct = 0,
+    incorrect = 0,
+    total = quizData.length;
+let questionBox = document.getElementById("questionBox");
+let allInputs = document.querySelectorAll("input[type='radio']")
+const loadQuestion = () => {
+    if (total === index) {
+        return quizEnd()
+    }
+    reset()
+    const data = quizData[index]
+    questionBox.innerHTML = `${index + 1}) ${data.question}`
+    allInputs[0].nextElementSibling.innerText = data.a
+    allInputs[1].nextElementSibling.innerText = data.b
+    allInputs[2].nextElementSibling.innerText = data.c
+    allInputs[3].nextElementSibling.innerText = data.d
+}
+
+document.querySelector("#submit").addEventListener(
+    "click",
+    function() {
+        const data = quizData[index]
+        const ans = getAnswer()
+        if (ans === data.correct) {
+            correct++;
+        } else {
+            incorrect++;
+        }
+        index++;
+        loadQuestion()
+    }
+)
+
+const getAnswer = () => {
+    let ans;
+    allInputs.forEach(
+        (inputEl) => {
+            if (inputEl.checked) {
+                ans = inputEl.value;
+            }
+        }
+    )
+    return ans;
+}
+
+const reset = () => {
+    allInputs.forEach(
+        (inputEl) => {
+            inputEl.checked = false;
+        }
+    )
+}
+
+const quizEnd = () => {
+    
+    document.getElementsByClassName("container")[0].innerHTML = `
+        <div class="col">
+            <h3 class="w-100"> Hii, you've scored ${correct} / ${total} </h3>
+        </div>
+    `
+}
+loadQuestion(index);
